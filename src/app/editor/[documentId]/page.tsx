5eb875abcbcb16addb34bdcb67fd202fd3a8f20a
raniel-roque <raniel.roque0509@gmail.com>
2025-06-26 12:41:51 +0800

    Added base editor

--- src/app/editor/[documentId]/page.tsx
+++ src/app/editor/[documentId]/page.tsx
@@ -8,10 +8,9 @@ interface DocumentIdPageProps {
 
 const DocumentIdPage = async ({ params }: DocumentIdPageProps) => {
   const { documentId } = await params;
-  
+
   return (
-    <div>
-      Document Id: {documentId}
+    <div className="min-h-screen bg-[#FAFBFD]">
       <Editor />
     </div>
   );
