59889da196443164100ddc61aadac7d8c6b2a9a1
raniel-roque <raniel.roque0509@gmail.com>
2025-09-13 21:44:39 +0800

    GAnalytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-FRHNS1CELR"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-FRHNS1CELR');
+            `,
+          }}
+        />
+      </head>
       <body className={inter.className}>
         <NuqsAdapter>
           <ConvexClientProvider>{children}</ConvexClientProvider>
